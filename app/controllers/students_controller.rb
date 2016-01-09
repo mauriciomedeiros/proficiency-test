a2fcaf9419e562ca9cf485d83244d94afe49bd36
Maurício Medeiros <mauriciomedeiros.05@gmail.com>
2016-01-09 18:11:28 -0200

    Implement student crud

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,2 +1,31 @@
 class StudentsController < ApplicationController
+   def index
+      @students = Student.order :name
+   end
+
+   def new
+      @student = Student.new
+   end
+
+   def create
+      @student = Student.new student_params
+      if @student.save
+         flash[:notice] = "Student was successfully created."
+			redirect_to root_url
+      else
+         render :new
+      end
+   end
+
+   def destroy
+		id = params[:id]
+		Student.destroy id
+		redirect_to root_url
+	end
+
+   private
+
+   def student_params
+      params.require(:student).permit :name, :register_number, :status
+   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-   get "students" => 'students#index'
+   root "students#index"
+   resources :students, only: [:new, :create, :destroy]
 end
